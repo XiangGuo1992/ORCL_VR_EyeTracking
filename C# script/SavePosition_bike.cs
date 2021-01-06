e8c437daecbe4569f2a33a1a435229d8900ddcc4
Xiang Guo <buaaguoxiang@126.com>
2021-01-06 17:48:35 -0500

    Add files via upload

--- C# script/SavePosition_bike.cs
+++ C# script/SavePosition_bike.cs
@@ -34,17 +34,47 @@ public class SavePosition_bike : MonoBehaviour
 
     // For the trigger
     // // a reference to the action
-    public SteamVR_Action_Boolean Trigger_press; //Grab Pinch is the trigger, select from inspecter
+    public SteamVR_Action_Boolean Trigger_press_left; //Grab Pinch is the trigger, select from inspecter
     // a reference to the hand
-    public SteamVR_Input_Sources TriggerHandType;
+    public SteamVR_Input_Sources TriggerHandLeft;
+    public SteamVR_Action_Boolean Trigger_press_right; //Grab Pinch is the trigger, select from inspecter
+    // a reference to the hand
+    public SteamVR_Input_Sources TriggerHandRight;
+
+    //for the trigger squeeze
+    public SteamVR_Action_Single Trigger_squeeze_left;
+    public SteamVR_Input_Sources Trigger_squeeze_HandLeft;
+    //for the trigger squeeze
+    public SteamVR_Action_Single Trigger_squeeze_right;
+    public SteamVR_Input_Sources Trigger_squeeze_HandRight;
 
     //for the grip
-    public SteamVR_Action_Boolean Grip_press;
-    public SteamVR_Input_Sources GripHandType;
+    public SteamVR_Action_Boolean Grip_press_left;
+    public SteamVR_Input_Sources GripHandLeft;
+    //for the grip
+    public SteamVR_Action_Boolean Grip_press_right;
+    public SteamVR_Input_Sources GripHandRight;
 
     //for the touchpad
-    public SteamVR_Action_Boolean Touchpad_press;
-    public SteamVR_Input_Sources TouchpadHandType;
+    public SteamVR_Action_Boolean Touchpad_press_left;
+    public SteamVR_Input_Sources Touchpadpress_HandLeft;
+    //for the touchpad
+    public SteamVR_Action_Boolean Touchpad_press_right;
+    public SteamVR_Input_Sources Touchpadpress_HandRight;
+
+    //for the touchpad
+    public SteamVR_Action_Boolean Touchpad_touch_left;
+    public SteamVR_Input_Sources Touchpadtouch_HandLeft;
+    //for the touchpad
+    public SteamVR_Action_Boolean Touchpad_touch_right;
+    public SteamVR_Input_Sources Touchpadtouch_HandRight;
+
+    //for the touchpad
+    public SteamVR_Action_Vector2 Touchpad_position_left;
+    public SteamVR_Input_Sources Touchpadposition_HandLeft;
+    //for the touchpad
+    public SteamVR_Action_Vector2 Touchpad_position_right;
+    public SteamVR_Input_Sources Touchpadposition_HandRight;
 
 
 
@@ -58,10 +88,20 @@ public class SavePosition_bike : MonoBehaviour
     GameObject car;
     GameObject VRU;
 
-    int Trigger_press_controller_state;
-    int Grip_press_controller_state;
-    int TouchPad_press_controller_state;
-    private Vector2 TouchPad_axis;
+    int Trigger_press_controller_state_left;
+    int Grip_press_controller_state_left;
+    int TouchPad_press_controller_state_left;
+    int TouchPad_touch_controller_state_left;
+    private Vector2 TouchPad_axis_left;
+    private float Trigger_squeeze_value_left;
+
+    int Trigger_press_controller_state_right;
+    int Grip_press_controller_state_right;
+    int TouchPad_press_controller_state_right;
+    int TouchPad_touch_controller_state_right;
+    private Vector2 TouchPad_axis_right;
+    private float Trigger_squeeze_value_right;
+
 
     // Start is called before the first frame update
     void Start()
@@ -79,31 +119,60 @@ public class SavePosition_bike : MonoBehaviour
         cam = VRU.GetComponent<Camera>();
         car = GameObject.Find("------Car Spawner");
 
-        Trigger_press_controller_state = 0;
-        Grip_press_controller_state = 0;
-        TouchPad_press_controller_state = 0;
-        TouchPad_axis = new Vector2(0.0f, 0.0f);
+        Trigger_press_controller_state_left = 0;
+        Grip_press_controller_state_left = 0;
+        TouchPad_press_controller_state_left = 0;
+        TouchPad_touch_controller_state_left = 0;
+        TouchPad_axis_left = new Vector2(0.0f, 0.0f);
+
+        Trigger_press_controller_state_right = 0;
+        Grip_press_controller_state_right = 0;
+        TouchPad_press_controller_state_right = 0;
+        TouchPad_touch_controller_state_right = 0;
+        TouchPad_axis_right = new Vector2(0.0f, 0.0f);
 
 
 
-        Trigger_press.AddOnStateDownListener(TriggerDown, TriggerHandType);
-        Trigger_press.AddOnStateUpListener(TriggerUp, TriggerHandType);
 
-        Grip_press.AddOnStateDownListener(GripDown,GripHandType);
-        Grip_press.AddOnStateUpListener(GripUp, GripHandType);
+        Trigger_press_left.AddOnStateDownListener(TriggerDownLeft, TriggerHandLeft);
+        Trigger_press_left.AddOnStateUpListener(TriggerUpLeft, TriggerHandLeft);
 
-        Touchpad_press.AddOnStateDownListener(TouchpadDown, TouchpadHandType);
-        Touchpad_press.AddOnStateUpListener(TouchpadUp, TouchpadHandType);
+        Trigger_press_right.AddOnStateDownListener(TriggerDownRight, TriggerHandRight);
+        Trigger_press_right.AddOnStateUpListener(TriggerUpRight, TriggerHandRight);
 
-        //TouchPad.AddOnAxisListener(TouchpadValue, TouchpadHandType);
+
+        Grip_press_left.AddOnStateDownListener(GripDownLeft, GripHandLeft);
+        Grip_press_left.AddOnStateUpListener(GripUpLeft, GripHandLeft);
+        Grip_press_right.AddOnStateDownListener(GripDownRight, GripHandRight);
+        Grip_press_right.AddOnStateUpListener(GripUpRight, GripHandRight);
+
+        Touchpad_press_left.AddOnStateDownListener(TouchpadDownLeft, Touchpadpress_HandLeft);
+        Touchpad_press_left.AddOnStateUpListener(TouchpadUpLeft, Touchpadpress_HandLeft);
+        Touchpad_press_right.AddOnStateDownListener(TouchpadDownRight, Touchpadpress_HandRight);
+        Touchpad_press_right.AddOnStateUpListener(TouchpadUpRight, Touchpadpress_HandRight);
+
+        Touchpad_touch_left.AddOnStateDownListener(TouchpadTouchDownLeft, Touchpadtouch_HandLeft);
+        Touchpad_touch_left.AddOnStateUpListener(TouchpadTouchUpLeft, Touchpadtouch_HandLeft);
+        Touchpad_touch_right.AddOnStateDownListener(TouchpadTouchDownRight, Touchpadtouch_HandRight);
+        Touchpad_touch_right.AddOnStateUpListener(TouchpadTouchUpRight, Touchpadtouch_HandRight);
+
+
+
+
+
+        //Touchpad_position.AddOnAxisListener(TouchpadValue, Touchpadposition_HandType);
 
 
 
 
 
         StreamWriter sw = File.AppendText(FILE_NAME);
-        sw.WriteLine("date, time, timestamp, data_time," + "camposition.x , camposition.y, camposition.z," + "Camera.facing.x," + "Camera.facing.y," + "Camera.facing.z," + "Trigger_press_controller_state,"
-            + "Grip_press_controller_state," + "TouchPad_press_controller_state," + "left_controller.x , left_controller.y, left_controller.z," + "right_controller.x , right_controller.y, right_controller.z,"
+        sw.WriteLine("date, time, timestamp, data_time," + "camposition.x , camposition.y, camposition.z," + "Camera.facing.x," + "Camera.facing.y," + "Camera.facing.z," 
+            + "Trigger_press_controller_state_left, Trigger_squeeze_value_left," + "Trigger_press_controller_state_right, Trigger_squeeze_value_right,"
+            + "Grip_press_controller_state_left, Grip_press_controller_state_right," + "TouchPad_press_controller_state_left,TouchPad_press_controller_state_right," 
+            + "TouchPad_touch_controller_state_left,TouchPad_touch_controller_state_right,"
+            + "TouchPad_axis_left.x , TouchPad_axis_left.y," + "TouchPad_axis_right.x , TouchPad_axis_right.y,"
+            + "left_controller.x , left_controller.y, left_controller.z," + "right_controller.x , right_controller.y, right_controller.z,"
             + "speed," + "elapsedTime," + "heartRate," + "distanceTraveled," + "instantaneousPower," + "cadence" 
 
             //"screenPospedestrianX,screenPospedestrianY, screenPospedestrianZ," +
@@ -119,46 +188,111 @@ public class SavePosition_bike : MonoBehaviour
     }
 
 
-    public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    public void TriggerUpLeft(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        Trigger_press_controller_state = 0;
-
+        Trigger_press_controller_state_left = 0;
     }
-    public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    public void TriggerDownLeft(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        Trigger_press_controller_state = 1;
-
-    }
-
-    public void GripUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-    {
-        Grip_press_controller_state = 0;
-
-    }
-    public void GripDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-    {
-        Grip_press_controller_state = 1;
+        Trigger_press_controller_state_left = 1;
 
     }
 
-    public void TouchpadUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    public void TriggerUpRight(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        TouchPad_press_controller_state = 0;
+        Trigger_press_controller_state_right = 0;
+    }
+    public void TriggerDownRight(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Trigger_press_controller_state_right = 1;
 
     }
-    public void TouchpadDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+
+
+
+ 
+
+
+    public void GripUpLeft(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        TouchPad_press_controller_state = 1;
+        Grip_press_controller_state_left = 0;
 
     }
+    public void GripDownLeft(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Grip_press_controller_state_left = 1;
+
+    }
+
+    public void GripUpRight(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Grip_press_controller_state_right = 0;
+
+    }
+    public void GripDownRight(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Grip_press_controller_state_right = 1;
+
+    }
+
+
+
+    public void TouchpadUpLeft(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        TouchPad_press_controller_state_left = 0;
+
+    }
+    public void TouchpadDownLeft(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        TouchPad_press_controller_state_left = 1;
+
+    }
+
+    public void TouchpadUpRight(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        TouchPad_press_controller_state_right = 0;
+
+    }
+    public void TouchpadDownRight(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        TouchPad_press_controller_state_right = 1;
+
+    }
+
+
+
+    public void TouchpadTouchUpLeft(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        TouchPad_touch_controller_state_left = 0;
+
+    }
+    public void TouchpadTouchDownLeft(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        TouchPad_touch_controller_state_left = 1;
+
+    }
+
+    public void TouchpadTouchUpRight(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        TouchPad_touch_controller_state_right = 0;
+
+    }
+    public void TouchpadTouchDownRight(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        TouchPad_touch_controller_state_right = 1;
+
+    }
+
+
 
     /*
-    public void TouchpadValue()
-    {
-        TouchPad_axis = TouchPad_axis.GetAxis();
+      public void TouchpadValue()
+      {
+          TouchPad_axis = Touchpad_position.GetAxis();
 
-    }
+      }
     */
+
 
 
     // Update is called once per frame
@@ -172,6 +306,13 @@ public class SavePosition_bike : MonoBehaviour
 
         Vector3 camposition =cam.transform.position;
         Vector3 camfacing = cam.transform.forward;
+
+        TouchPad_axis_left = Touchpad_position_left.GetAxis(Touchpadposition_HandLeft);
+        TouchPad_axis_right = Touchpad_position_right.GetAxis(Touchpadposition_HandRight);
+        //Trigger_squeeze_value = Trigger_squeeze.GetAxis(Trigger_squeeze_HandType);
+        Trigger_squeeze_value_left = Trigger_squeeze_left.GetAxis(Trigger_squeeze_HandLeft);
+        Trigger_squeeze_value_right = Trigger_squeeze_right.GetAxis(Trigger_squeeze_HandRight);
+
 
         Vector3 left_controller = GameObject.Find("-----Bicycle/Indicator/Player/SteamVRObjects/LeftHand").transform.position;
         Vector3 right_controller = GameObject.Find("-----Bicycle/Indicator/Player/SteamVRObjects/RightHand").transform.position;
@@ -194,12 +335,15 @@ public class SavePosition_bike : MonoBehaviour
         //Vector3 screenPosSphere = cam.WorldToScreenPoint(sphere.transform.position);
 
 
-
-
-        StreamWriter sw = File.AppendText(FILE_NAME);
+StreamWriter sw = File.AppendText(FILE_NAME);
 
         sw.WriteLine(date + "," + time + "," + timestamp + "," + data_time + "," + camposition.x + "," + camposition.y + "," + camposition.z + ","
-             + camfacing.x + "," + camfacing.y + "," + camfacing.z + "," + Trigger_press_controller_state + "," + Grip_press_controller_state + "," + TouchPad_press_controller_state + ","
+             + camfacing.x + "," + camfacing.y + "," + camfacing.z + ","             
+             + Trigger_press_controller_state_left + "," + Trigger_squeeze_value_left + "," + Trigger_press_controller_state_right + "," + Trigger_squeeze_value_right + ","
+             + Grip_press_controller_state_left + "," + Grip_press_controller_state_right + ","               
+             + TouchPad_press_controller_state_left + "," + TouchPad_press_controller_state_right + ","              
+             + TouchPad_touch_controller_state_left + "," + TouchPad_touch_controller_state_right + ","
+             + TouchPad_axis_left.x + "," + TouchPad_axis_left.y + "," + TouchPad_axis_right.x + "," + TouchPad_axis_right.y + ","
              + left_controller.x + "," + left_controller.y + "," + left_controller.z + "," + right_controller.x + "," + right_controller.y + "," + right_controller.z + ","
 
               + speed + "," + elapsedTime + "," + heartRate + "," + distanceTraveled + "," + instantaneousPower + "," + cadence 
@@ -215,6 +359,6 @@ public class SavePosition_bike : MonoBehaviour
 
         sw.Close();
 
-        //Debug.Log("write to file");
+        //Debug.Log("trigger_press_state:" + Trigger_press_controller_state + "trigger_press_value:" + Trigger_squeeze_value.ToString("F"));
     }
 }
